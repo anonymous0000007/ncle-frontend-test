58b3444d3813ea1d21a0e9cba96152b3bb984421
Rushil <shahrushil09@gmail.com>
2024-03-15 16:14:03 -0400

    ENHANCEMENT: subscription component subscribe card updated v4

--- src/app/subscription/subscription.component.ts
+++ src/app/subscription/subscription.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Platform } from '@ionic/angular';
+import { Month } from '../enums/months.enum';
 
 @Component({
   selector: 'app-subscription',
@@ -7,5 +9,29 @@ import { Component } from '@angular/core';
 })
 export class SubscriptionComponent {
 
-  constructor() { }
+  public topics: string[] = ['Arabic', 'Islamic', 'English', 'History', 'Sports'];
+  public months: string[] = Object.keys(Month);
+  public categories: string[] = ['Arabic', 'Islamic', 'English', 'History', 'Sports']
+
+
+  constructor(public platform: Platform) { }
+
+  /**
+   * @returns {number}
+   * @param {number} index
+   * @description track by index
+   */
+  public trackByIndex(index: number): number {
+    return index;
+  }
+
+  /**
+   * @public
+   * @returns {number[]}
+   * @param {number} length
+   * @description number generator for ngFor
+   */
+  public generateNumbersArray(length: number): number[] {
+    return Array.from({ length }, (_: any, index: number): number => index + 1);
+  }
 }
