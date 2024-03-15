c9c6d2bea9a4a19e088230df5771d51c71d60a21
Rushil <shahrushil09@gmail.com>
2024-03-15 08:27:32 -0400

    ENHANCEMENT: webp fallback support

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Platform } from '@ionic/angular';
 
 @Component({
@@ -6,8 +6,26 @@ import { Platform } from '@ionic/angular';
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
+export class HomePage implements OnInit {
 
-  constructor(public platform: Platform) {}
 
+  /* public: variables */
+  /* image url to check support of webp images */
+  public surprisebackURL: string = 'assets/surpriseback.png';
+  public placeholderURL: string = 'assets/placeholder.png';
+
+
+  constructor(public platform: Platform) { }
+
+  ngOnInit(): void {
+    const canvas: HTMLCanvasElement = document.createElement('canvas');
+
+    if (
+      !!(canvas.getContext && canvas.getContext('2d')) &&
+      canvas.toDataURL('image/webp').indexOf('data:image/webp') === 0
+    ) {
+      this.surprisebackURL = 'assets/surpriseback.webp';
+      this.placeholderURL = 'assets/placeholder.webp';
+    }
+  }
 }
